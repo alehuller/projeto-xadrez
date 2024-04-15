cf3f5f35862037ad3ecc0870adcb852c9b029c72
Alejandro <ale_huller@hotmail.com>
2024-04-15 16:20:35 -0300

    Implementando tabuleiro e peca

---
+++ src/boardgame/Tabuleiro.java
@@ -0,0 +1,32 @@
+package boardgame;
+
+public class Tabuleiro {
+	private int rows;
+	private int columns;
+	private Peca[][] pieces;
+	
+	public Tabuleiro(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Peca[rows][columns];
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	
+	
+}
