3a297c556fdc3a8efdf61e06166276bed455f048
Alejandro <ale_huller@hotmail.com>
2024-04-16 15:42:53 -0300

    Tratando excecoes e dando clear na tela

--- src/application/UI.java
+++ src/application/UI.java
@@ -8,6 +8,11 @@ import chess.ChessPosition;
 
 public class UI {
 	
+	public static void clearScreen() {
+		System.out.println("\033[H\033[2J");
+		System.out.flush();
+	}
+	
 	public static ChessPosition readChessPosition(Scanner scanner) {
 		try {
 			String s = scanner.nextLine();
